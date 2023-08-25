497ab4b5c1a8614bc1560b05e5362b0110aea5aa
Jong Hak Kim <whdgkr9070@naver.com>
2023-08-25 22:36:07 +0900

    [Silver V] Title: 소트인사이드, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -6,15 +6,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
-        int N = Integer.parseInt(bufferedReader.readLine());
-        String strN = "" + N;
-        char[] array = new char[strN.length()];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = strN.charAt(i);
-        }
+        char[] array = bufferedReader.readLine().toCharArray();
         Arrays.sort(array);
-        stringBuilder.append(array).reverse();
-        System.out.println(stringBuilder);
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i]);
+        }
     }
 }

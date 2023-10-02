798369c81df38a2f1721b6f8fe74d64e256fd3dc
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-02 16:53:14 +0900

    [Silver IV] Title: 카드2, Time: 216 ms, Memory: 29068 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int result = 0;
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // ArrayList에 카드 넣기
+        for (int i = 1; i < N + 1; i++) {
+            deque.add(i);
+        }
+        // 동작 실행
+        for (int i = 0; i < N; i++) {
+            if (deque.size() == 1) {
+                result = deque.getFirst();
+                break;
+            }
+            // 맨 윗장 버림
+            deque.removeFirst();
+
+            // 맨 윗카드 맨 아래로 추가
+            deque.addLast(deque.getFirst());
+
+            // 맨 윗카드 삭제
+            deque.removeFirst();
+
+        }
+
+        System.out.println(result);
+    }
+}

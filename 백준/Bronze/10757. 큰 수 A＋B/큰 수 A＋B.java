164bdb0e1fa8f034d7cb3b58628c4d7a5ce063e6
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-05 22:55:00 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 244 ms, Memory: 17460 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        BigInteger A = new BigInteger(stringTokenizer.nextToken());
+        BigInteger B = new BigInteger(stringTokenizer.nextToken());
+        System.out.println(A.add(B));
+    }
+}

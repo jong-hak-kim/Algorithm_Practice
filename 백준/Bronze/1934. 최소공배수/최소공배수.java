69d2424c3a7481afcaf0b2be9f639a0e39167b14
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-10 21:07:33 +0900

    [Bronze I] Title: 최소공배수, Time: 148 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int A = Integer.parseInt(stringTokenizer.nextToken());
+            int B = Integer.parseInt(stringTokenizer.nextToken());
+            stringBuilder.append((A * B) / getGCD(A, B)).append("\n");
+        }
+        System.out.println(stringBuilder);
+    }
+
+    public static int getGCD(int A, int B) {
+        if (A % B == 0) {
+            return B;
+        }
+        return getGCD(B, A % B);
+    }
+}

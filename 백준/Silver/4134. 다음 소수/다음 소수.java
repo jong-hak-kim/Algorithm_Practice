8dbb0fca352f3749211e620fcd994561cbe6c2c9
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-11 19:30:46 +0900

    [Silver IV] Title: 다음 소수, Time: 456 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,14 +9,28 @@ public class Main {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < T; i++) {
             long num = Long.parseLong(bufferedReader.readLine());
-            BigInteger bi = new BigInteger(String.valueOf(num));
-            if (bi.isProbablePrime(100)) {
-                stringBuilder.append(num).append("\n");
-            } else {
-                stringBuilder.append(bi.nextProbablePrime()).append("\n");
+            while (true) {
+                if (!CheckPrime(num)) {
+                    num++;
+                }
+                if (CheckPrime(num)) {
+                    stringBuilder.append(num).append("\n");
+                    break;
+                }
             }
-
         }
         System.out.println(stringBuilder);
+    }
+
+    public static boolean CheckPrime(long num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }

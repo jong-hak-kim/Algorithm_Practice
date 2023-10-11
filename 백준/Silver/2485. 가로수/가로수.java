f9a01dcd192b79686a325bccefdaa4232e7f41b2
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-11 17:24:17 +0900

    [Silver IV] Title: 가로수, Time: 240 ms, Memory: 23116 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -7,34 +7,17 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
         int[] array = new int[N];
-        int[] distance = new int[N - 1];
         for (int i = 0; i < N; i++) {
             array[i] = Integer.parseInt(bufferedReader.readLine());
         }
 
-        for (int i = 1; i < N; i++) {
-            distance[i - 1] = array[i] - array[i - 1];
+        int gcd = 0;
+        for (int i = 0; i < N - 1; i++) {
+            int distance = array[i + 1] - array[i];
+            gcd = getGCD(gcd, distance);
         }
 
-        int lastGCD = 0;
-        int result = 0;
-        for (int i = 1; i < distance.length; i++) {
-            if (i == 1) {
-                result = getGCD(distance[i - 1], distance[i]);
-                lastGCD = result;
-                continue;
-            }
-            result = getGCD(lastGCD, distance[i]);
-        }
-
-        int count = 0;
-        for (int i = 0; i < distance.length; i++) {
-            if (distance[i] >= (result * 2)) {
-                count += (distance[i] / result) - 1;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println((array[N - 1] - array[0]) / gcd + 1 - N);
     }
 
     public static int getGCD(int A, int B) {

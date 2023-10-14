d30cd2879dbd8ec00f717e7235d99905615a732d
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-14 21:06:34 +0900

    [Silver IV] Title: 스택 2, Time: 940 ms, Memory: 135624 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -2,51 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder stringBuilder = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
-        Stack<Integer> stack = new Stack<>();
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer;
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int command = Integer.parseInt(stringTokenizer.nextToken());
-            if (command == 1) {
-                int X = Integer.parseInt(stringTokenizer.nextToken());
-                stack.push(X);
-            }
-            if (command == 2) {
-                if (stack.isEmpty()) {
-                    stringBuilder.append(-1).append("\n");
-                    continue;
-                }
-                stringBuilder.append(stack.pop()).append("\n");
 
+        for (int i = 0; i < N; i++) {
+            String select = bufferedReader.readLine();
+            Solution(select);
+        }
+        System.out.println(stringBuilder);
+    }
+
+    static void Solution(String select) {
+        char command = select.charAt(0);
+        if (command == '1') {
+            int X = Integer.parseInt(select.substring(2));
+            stack.push(X);
+        }
+        if (command == '2') {
+            if (stack.isEmpty()) {
+                stringBuilder.append(-1).append("\n");
+                return;
             }
-            if (command == 3) {
-                stringBuilder.append(stack.size()).append("\n");
+            stringBuilder.append(stack.pop()).append("\n");
+
+        }
+        if (command == '3') {
+            stringBuilder.append(stack.size()).append("\n");
+        }
+        if (command == '4') {
+            if (!stack.isEmpty()) {
+                stringBuilder.append(0).append("\n");
+                return;
             }
-            if (command == 4) {
-                if (!stack.isEmpty()) {
-                    stringBuilder.append(0).append("\n");
-                    continue;
-                }
-                if (stack.isEmpty()) {
-                    stringBuilder.append(1).append("\n");
-                }
-            }
-            if (command == 5) {
-                if (stack.isEmpty()) {
-                    stringBuilder.append(-1).append("\n");
-                    continue;
-                }
-                stringBuilder.append(stack.peek()).append("\n");
+            if (stack.isEmpty()) {
+                stringBuilder.append(1).append("\n");
             }
         }
-
-        System.out.println(stringBuilder);
+        if (command == '5') {
+            if (stack.isEmpty()) {
+                stringBuilder.append(-1).append("\n");
+                return;
+            }
+            stringBuilder.append(stack.peek()).append("\n");
+        }
     }
 }

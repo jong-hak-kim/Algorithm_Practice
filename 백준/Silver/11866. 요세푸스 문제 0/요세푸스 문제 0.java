a814006f17a5cd5dac3bdfb12a0c334f303f351e
Jong Hak Kim <whdgkr9070@naver.com>
2023-10-18 22:47:06 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 132 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,50 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        StringBuilder stringBuilder = new StringBuilder();
+
+        LinkedList<Integer> list = new LinkedList<>();
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int K = Integer.parseInt(stringTokenizer.nextToken());
-        Deque<Integer> josephus = new ArrayDeque<>();
-        List<Integer> numberList = new ArrayList<>();
-        int count = 0;
 
         for (int i = 1; i <= N; i++) {
-            numberList.add(i);
+            list.add(i);
         }
-        while (numberList.size() != 0) {
 
-            for (int i = 0; i < numberList.size(); i++) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<");
 
-                count++;
-                if (count == K) {
-                    josephus.add(numberList.get(i));
-                    numberList.remove(i);
-                    count = 0;
-                    i--;
-                }
-            }
+        int index = 0;
+
+        while (N > 1) {
+            index = (index + (K - 1)) % N;
+
+            stringBuilder.append(list.remove(index)).append(", ");
+            N--;
         }
-        int josephusSize = josephus.size();
-        for (int i = 0; i < josephusSize; i++) {
-            if (i == 0) {
-                stringBuilder.append("<" + josephus.getFirst());
-                josephus.removeFirst();
-            } else {
-                stringBuilder.append(", " + josephus.getFirst());
-                josephus.removeFirst();
-            }
-        }
-        stringBuilder.append(">");
+
+        stringBuilder.append(list.remove()).append(">");
         System.out.println(stringBuilder);
 
     }

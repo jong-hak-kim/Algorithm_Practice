0b8c35e86089af0a0fdada8a0909f6f7316c9982
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-11 22:04:53 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 120 ms, Memory: 13820 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("    8888888888  888    88888").append("\n");
+        stringBuilder.append("   88     88   88 88   88  88").append("\n");
+        stringBuilder.append("    8888  88  88   88  88888").append("\n");
+        stringBuilder.append("       88 88 888888888 88   88").append("\n");
+        stringBuilder.append("88888888  88 88     88 88    888888").append("\n");
+        stringBuilder.append("\n");
+        stringBuilder.append("88  88  88   888    88888    888888").append("\n");
+        stringBuilder.append("88  88  88  88 88   88  88  88").append("\n");
+        stringBuilder.append("88 8888 88 88   88  88888    8888").append("\n");
+        stringBuilder.append(" 888  888 888888888 88  88      88").append("\n");
+        stringBuilder.append("  88  88  88     88 88   88888888");
+        System.out.println(stringBuilder);
+    }
+}

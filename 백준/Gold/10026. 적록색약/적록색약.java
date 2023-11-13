5f3a18512d18120253b12c3ed1955f7aa46092b5
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-13 19:14:54 +0900

    [Gold V] Title: 적록색약, Time: 172 ms, Memory: 19252 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static char[][] graph;
+    public static boolean[][] visited;
+    public static boolean[][] blindVisited;
+    public static int N;
+    public static int count = 0;
+    public static int blindCount = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bufferedReader.readLine());
+        graph = new char[N][N];
+        visited = new boolean[N][N];
+        blindVisited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String input = bufferedReader.readLine();
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = input.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    count++;
+                    dfs(i, j, graph[i][j]);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!blindVisited[i][j]) {
+                    blindCount++;
+                    blindDfs(i, j, graph[i][j]);
+                }
+            }
+        }
+
+        System.out.println(count + " " + blindCount);
+    }
+
+    public static void dfs(int startX, int startY, char color) {
+        visited[startX][startY] = true;
+        int[] X = { 0, 0, -1, 1 };
+        int[] Y = { -1, 1, 0, 0 };
+        for (int i = 0; i < 4; i++) {
+            int x = startX + X[i];
+            int y = startY + Y[i];
+            if (x < 0 || x >= N || y < 0 || y >= N) {
+                continue;
+            }
+            if (graph[x][y] == color && !visited[x][y]) {
+                dfs(x, y, color);
+            }
+        }
+    }
+
+    public static void blindDfs(int startX, int startY, char color) {
+        blindVisited[startX][startY] = true;
+        int[] X = { 0, 0, -1, 1 };
+        int[] Y = { -1, 1, 0, 0 };
+        for (int i = 0; i < 4; i++) {
+            int x = startX + X[i];
+            int y = startY + Y[i];
+            if (x < 0 || x >= N || y < 0 || y >= N) {
+                continue;
+            }
+            if ((color == 'R' || color == 'G') && (graph[x][y] == 'R' || graph[x][y] == 'G') && !blindVisited[x][y]) {
+                blindDfs(x, y, color);
+            }
+            if (graph[x][y] == color && !blindVisited[x][y]) {
+                blindDfs(x, y, color);
+            }
+        }
+    }
+}

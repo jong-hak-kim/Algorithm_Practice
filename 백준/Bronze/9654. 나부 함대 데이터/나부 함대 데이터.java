4b9cb5710b82e4a9c47bf99c5b2499dc2c1947fb
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-14 21:13:51 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(
+                "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         ");
+        System.out.println(stringBuilder);
+    }
+}

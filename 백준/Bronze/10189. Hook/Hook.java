471ba3f17050d381f0b63b30212ad44e7e195f3b
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-15 19:47:18 +0900

    [Bronze V] Title: Hook, Time: 116 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+        System.out.println(stringBuilder);
+    }
+}

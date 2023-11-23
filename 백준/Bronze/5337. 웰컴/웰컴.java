83685ca60b5bb4e12427ebb85a19710c7ddea94d
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-23 19:16:51 +0900

    [Bronze V] Title: 웰컴, Time: 124 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(".  .   .").append("\n").append("|  | _ | _. _ ._ _  _").append("\n")
+                .append("|/\\|(/.|(_.(_)[ | )(/.");
+
+        System.out.println(stringBuilder);
+    }
+}

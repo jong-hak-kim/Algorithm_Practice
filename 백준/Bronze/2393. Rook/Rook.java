d9f9eaad3a1acb62a7d8b54c46d0e3823f08dd3d
Jong Hak Kim <whdgkr9070@naver.com>
2023-11-26 17:32:39 +0900

    [Bronze V] Title: Rook, Time: 108 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(
+                "  ___  ___  ___\n  | |__| |__| |\n  |           |\n   \\_________/\n    \\_______/\n     |     |\n     |     |\n     |     |\n     |     |\n     |_____|\n  __/       \\__\n /             \\\n/_______________\\");
+        System.out.println(stringBuilder);
+    }
+}

97f6a6976e8d9e93dd038cda4571adcbab0a75a5
Jong Hak Kim <whdgkr9070@naver.com>
2024-01-22 21:12:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 488 ms, Memory: 87864 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] num = new int[N];
-		int max = 0;
-		int min = 0;
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < num.length; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-			if (num[i] > max || i == 0) {
-				max = num[i];
-			}
-			if (num[i] < min || i == 0) {
-				min = num[i];
-			}
-		}
-		System.out.println(min + " " + max);
-	}
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int num = Integer.parseInt(stringTokenizer.nextToken());
+        int max = num;
+        int min = num;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < N; i++) {
+            num = Integer.parseInt(stringTokenizer.nextToken());
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+        }
+        stringBuilder.append(min + " " + max);
+        System.out.println(stringBuilder.toString());
+    }
 }

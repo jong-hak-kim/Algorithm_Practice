09921b3cc0529528ef26112d786d730789fbbb9b
Jong Hak Kim <whdgkr9070@naver.com>
2024-01-25 19:57:56 +0900

    [Bronze I] Title: 단어 공부, Time: 268 ms, Memory: 20148 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String words = br.readLine();
+		char[] alphabet = new char[26]; // 알파벳 나온 개수가 담길 배열
+		for (int i = 0; i < words.length(); i++) {
+			if ('A' <= words.charAt(i) && words.charAt(i) <= 'Z') { // 대문자일 경우
+				alphabet[words.charAt(i) - 'A']++;
+			}
+
+			else { // 소문자일 경우
+				alphabet[words.charAt(i) - 'a']++;
+			}
+		}
+		int max = 0;
+		char maxalphabet = 0;
+		for (int i = 0; i < alphabet.length; i++) {
+			if (alphabet[i] > max || i == 0) {
+				max = alphabet[i];
+				maxalphabet = (char) (i + 'A');
+			} else if (alphabet[i] == max) {
+				maxalphabet = '?';
+			}
+		}
+		System.out.println(maxalphabet);
+
+	}
+}

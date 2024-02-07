30a95542cb4a76a3e3603a790a246e034ba665f2
Jong Hak Kim <whdgkr9070@naver.com>
2024-02-07 21:20:46 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 1216 ms, Memory: 35844 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static char[][] stars;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        //1, 5, 9, 13
+        // 4n - 3
+        // (2,2) (4,4) (6,6)
+        int N = Integer.parseInt(bufferedReader.readLine());
+        stars = new char[(4 * N) - 3][(4 * N) - 3];
+
+        int centerX = 2 * (N - 1);
+        int centerY = 2 * (N - 1);
+        int length = (4 * N) - 3;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                stars[i][j] = ' ';
+            }
+        }
+        stars[centerX][centerY] = '*';
+        makeStar(0, length);
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                System.out.print(stars[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void makeStar(int start, int length) {
+        if (length <= start) return;
+        for (int i = start; i < length; i++) {
+            stars[i][length - 1] = '*';
+            stars[start][i] = '*';
+            stars[i][start] = '*';
+            stars[length - 1][i] = '*';
+        }
+
+        makeStar(start + 2, length - 2);
+
+    }
+}

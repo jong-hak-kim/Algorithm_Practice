20ed963cbd25b79f2c66b8fb64c1c0a590a611e5
Jong Hak Kim <whdgkr9070@naver.com>
2024-03-01 20:28:22 +0900

    [Silver IV] Title: 큐, Time: 200 ms, Memory: 19172 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        int N = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            String command = stringTokenizer.nextToken();
+            if(command.equals("push")){
+                int number = Integer.parseInt(stringTokenizer.nextToken());
+                deque.push(number);
+                continue;
+            }
+            if(command.equals("pop")){
+                if(deque.isEmpty()){
+                    stringBuilder.append("-1").append("\n");
+                    continue;
+                }
+                stringBuilder.append(deque.pollLast()).append("\n");
+                continue;
+            }
+
+            if(command.equals("size")){
+                stringBuilder.append(deque.size()).append("\n");
+                continue;
+            }
+
+            if(command.equals("empty")){
+                if(deque.isEmpty()){
+                    stringBuilder.append(1).append("\n");
+                    continue;
+                }
+                stringBuilder.append(0).append("\n");
+                continue;
+            }
+
+            if(command.equals("front")){
+                if(deque.isEmpty()){
+                    stringBuilder.append(-1).append("\n");
+                    continue;
+                }
+                stringBuilder.append(deque.peekLast()).append("\n");
+                continue;
+            }
+
+            if(command.equals("back")){
+                if(deque.isEmpty()){
+                    stringBuilder.append(-1).append("\n");
+                    continue;
+                }
+                stringBuilder.append(deque.peekFirst()).append("\n");
+            }
+        }
+
+        System.out.println(stringBuilder.toString());
+    }
+}

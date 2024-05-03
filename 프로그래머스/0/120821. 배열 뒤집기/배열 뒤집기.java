db6c9953e964420547f7ba0e613b6e3cd3864c43
Jong Hak Kim <whdgkr9070@naver.com>
2024-05-03 16:23:43 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.15 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,11 +1,12 @@
-import java.util.List;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Integer[] result = Arrays.stream(num_list).boxed().toArray(Integer[]::new);
-        List<Integer> list = Arrays.asList(result);
+
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
         Collections.reverse(list);
         return list.stream().mapToInt(Integer::intValue).toArray();
     }

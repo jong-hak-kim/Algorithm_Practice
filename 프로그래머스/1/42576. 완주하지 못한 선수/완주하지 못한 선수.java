1d93daa36429a7f118ecc20e4ec3cd2a5311fde3
Jong Hak Kim <whdgkr9070@naver.com>
2024-05-29 23:57:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.86 ms, Memory: 96.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,28 @@
+//HashMap에 참여 선수 이름과 인원 추가
+// 키 = 선수 이름, 값 = 인원 수
+// 완주한 선수 이름 배열 크기만큼 반복하여 제거
+    // 해쉬맵의 값이 1보다 작아질 경우
+    // 버킷 제거
+
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+       HashMap<String, Integer> players = new HashMap<>();
+        for (String player : participant) {
+            players.put(player, players.getOrDefault(player, 0) + 1);
+        }
+
+
+        for (String complete : completion) {
+            if (players.containsKey(complete)) {
+                players.put(complete, players.get(complete) - 1);
+                if (players.get(complete) < 1) {
+                    players.remove(complete);
+                }
+            }
+        }
+        return players.keySet().toString().replace("[", "").replace("]", "");
+        
+    }
+}

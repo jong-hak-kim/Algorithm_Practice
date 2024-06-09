2293007f2306635e08b5f8203bece2cf6dbeb4e0
Jong Hak Kim <whdgkr9070@naver.com>
2024-06-09 20:52:01 +0900

    [level 3] Title: 베스트앨범, Time: 3.70 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,6 +1,14 @@
-// 노래 재생이 많이 된 장르
-// 장르 내에서 가장 많이 재생된 노래 나열
+// 노래 재생이 많이 된 장르 계산
+// 장르 내에서 가장 많이 재생된 노래 2곡 나열
     // 재생 횟수 같으면 인덱스 낮은 노래 먼저 수록
+
+// 해쉬맵<장르, 재생 수> genreCount
+// 해쉬맵<장르, 배열[인덱스, 재생 수]> songs
+// 장르별 재생 수를 계산한 genreCount 해쉬맵을 내림차순 정렬한다.
+// 많은 재생 수 장르 순서대로 꺼내어 songs 해쉬맵을 내림차순으로 정렬하여 각 장르별 최대 2곡 answer에 저장
+    //이렇게 하면 재생 수 많은 장르에 있는 노래 우선으로 저장된다.
+    //순서가 있게 정렬을 하게 되면 같은 노래들은 기존 순서대로 따라가게 되는 stable sort를 하게 된다.
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,7 +19,7 @@ import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> genreCount = new HashMap<>();
+ HashMap<String, Integer> genreCount = new HashMap<>();
         HashMap<String, ArrayList<int[]>> songs = new HashMap<>();
 
         // 장르별 재생횟수 많은 순 계산
@@ -30,15 +38,20 @@ class Solution {
 
         List<Integer> answer = new ArrayList<>();
 
-        Stream<Map.Entry<String, Integer>> sortedGenre = genreCount.entrySet().stream()
-                .sorted((o1, o2) -> Integer.compare(o2.getValue(), o1.getValue()));
+        List<String> keySet = new ArrayList<>(genreCount.keySet());
 
-        sortedGenre.forEach(entry -> {
-            Stream<int[]> sortedSongs = songs.get(entry.getKey()).stream().sorted((o1, o2) -> Integer.compare(o2[1], o1[1]))
-                    .limit(2);
-            sortedSongs.forEach(song -> answer.add(song[0]));
-        });
+        keySet.sort((o1, o2) -> genreCount.get(o2).compareTo(genreCount.get(o1)));
 
+        for (String key : keySet) {
+            songs.get(key).sort((o1, o2) -> o2[1] - o1[1]);
+            if (songs.get(key).size() > 1) {
+                for (int i = 0; i < 2; i++) {
+                    answer.add(songs.get(key).get(i)[0]);
+                }
+            } else {
+                answer.add(songs.get(key).get(0)[0]);
+            }
+        }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }

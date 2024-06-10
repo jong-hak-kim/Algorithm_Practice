5eb1c9deb17cf43dffad1fc492a168f324a29b16
Jong Hak Kim <whdgkr9070@naver.com>
2024-06-10 13:03:00 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.14 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,18 +1,17 @@
-// 정지된 유저 확인
-// 유저가 신고한 ID 확인
-    // 그 중 정지된 ID 확인
-
-//해쉬맵<유저 ID, 신고 List>
-    //순회하며 정지된 유저 수 반환
+//해쉬맵<신고당한 유저 ID, 신고한 유저들>
+    //순회하며 정지당한 유저는 결과 해쉬맵 저장
+    //저장 안된 유저들은 0으로 저장
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, Integer> reportMap = new HashMap<>();
-        HashMap<String, List<String>> reportUsers = new HashMap<>();
+        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
+        HashMap<String, Integer> count = new HashMap<>();
 
         //신고당한 횟수 계산
         for (String userReportLog : report) {
@@ -20,45 +19,28 @@ class Solution {
             String user = log[0];
             String reportUser = log[1];
 
-            if (!reportUsers.containsKey(user)) {
-                reportUsers.put(user, new ArrayList<>());
+            if (!reportMap.containsKey(reportUser)) {
+                reportMap.put(reportUser, new HashSet<>());
             }
 
-            if (!reportUsers.get(user).contains(reportUser)) {
-                reportMap.put(reportUser, reportMap.getOrDefault(reportUser, 0) + 1);
-                reportUsers.get(user).add(reportUser);
-            }
+            reportMap.get(reportUser).add(user);
         }
 
-        List<String> keySet = new ArrayList<>(reportMap.keySet());
-
-        List<String> banUsers = new ArrayList<>();
-
-        //정지당한 유저 구하기
-        for (String key : keySet) {
-            if (reportMap.get(key) >= k) {
-                banUsers.add(key);
-            }
-        }
-
-        List<Integer> answer = new ArrayList<>();
-
-        for (String id : id_list) {
-            List<String> reportUser = reportUsers.get(id);
-            int count = 0;
-            if (reportUser == null) {
-                answer.add(0);
-                continue;
-            }
-            for (String key : banUsers) {
-                if (reportUser.contains(key)) {
-                    count++;
+        for (Map.Entry<String, HashSet<String>> entry : reportMap.entrySet()) {
+            if (entry.getValue().size() >= k) {
+                for (String userId : entry.getValue()) {
+                    count.put(userId, count.getOrDefault(userId, 0) + 1);
                 }
             }
-            answer.add(count);
         }
 
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[id_list.length];
+
+        for (int i = 0; i < id_list.length; i++) {
+            answer[i] = count.getOrDefault(id_list[i], 0);
+        }
+
+        return answer;
         
     }
 }

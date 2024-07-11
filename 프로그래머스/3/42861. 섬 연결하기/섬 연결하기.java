d99372313c9fcab852f1149d0575f2b65d7e6580
Jong Hak Kim <whdgkr9070@naver.com>
2024-07-11 20:48:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.45 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,56 @@
+//최소의 경로 비용
+//루트 노드 찾기
+//유니온
+//비용 오름차순 정렬
+//작은 다리부터 연결
+//사이클 여부 확인
+// 다리는 n - 1개 연결
+import java.util.Arrays;
+class Solution {
+    
+    private static int[] parent;
+    
+        private static int find(int x){
+        if(parent[x] == x) {
+            return x;
+        }
+        
+        return parent[x] = find(parent[x]);
+    }
+    
+    private static void union(int x, int y) {
+        int root1 = find(x);
+        int root2 = find(y);
+        parent[root2] = root1;
+    }
+    
+    public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+        
+        parent = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        
+        int bridgeCount = 0;
+        int answer = 0;
+        
+        for(int[] edge : costs){
+            if(bridgeCount == n - 1){
+                break;
+            }
+            
+            if(find(edge[0]) != find(edge[1])) {
+                union(edge[0], edge[1]);
+                answer += edge[2];
+                bridgeCount++;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+
+}
